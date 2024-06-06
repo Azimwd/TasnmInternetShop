72a68de2d1395f5ba04ac4a9fa67cefb782554bc
Azik Temirbolat <programmingsince2023@gmail.com>
2024-06-06 13:10:50 +0500

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def singleproduct(request):
+    return render(request, 'single-product.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+def registration(request):
+    return render(request, 'registration.html')
+
+def login(request):
+    return render(request, 'login.html')
+
