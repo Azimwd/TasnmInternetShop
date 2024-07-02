5239b8a6f607784c3013d8fd3fb4767fbb9a8c6f
Azik Temirbolat <programmingsince2023@gmail.com>
2024-07-02 16:12:56 +0500

    added categories filter

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -4,7 +4,7 @@ from goods.views import *
 app_name = 'goods'
 
 urlpatterns = [
-    path('', catalog, name = 'index'),
+    path('<slug:category_slug>/', catalog, name = 'index'),
     path('product/<slug:product_slug>/', product, name = 'product'),
     # path("singleproduct/", singleproduct, name = "singleproduct"),  
 ]
--- app/goods/views.py
+++ app/goods/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from goods.models import Products
 
-def catalog(request):
-    products = Products.objects.all()
+
+def catalog(request, category_slug):
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+    
+        
     context = {
-        'products': products,
+        "goods": goods,
     }
-    return render(request, "goods/shop.html",context)
+    return render(request, "goods/shop.html", context)
 
 
 def product(request, product_slug):
-    productslug = Products.objects.get(slug = product_slug)
+    productslug = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': productslug
-    }
-    return render(request, "goods/product.html", context = context)
+    context = {"product": productslug}
+    return render(request, "goods/product.html", context=context)
