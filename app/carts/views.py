d79f3fb23494239da9e8fc0981f181947b869b1c
Azik Temirbolat <programmingsince2023@gmail.com>
2024-09-11 13:52:22 +0500

    added session

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,5 +1,8 @@
 
+from django.contrib import sessions
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from carts.models import Cart
 from goods.models import Categories
@@ -20,11 +23,38 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity = 1)
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product )
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(session_key=request.session.session_key, product=product, quantity=1)
 
     return redirect(request.META['HTTP_REFERER'], goods)
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
 
 def cart_remove(request, cart_id):
 
